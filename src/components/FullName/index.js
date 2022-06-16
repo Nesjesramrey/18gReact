368ddeb6f1f4368c1f2d6a2c264e3dbc8a44f04a
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-06-15 20:14:24 -0500

    react

---
+++ src/components/FullName/index.js
@@ -0,0 +1,8 @@
+import './fullName.css';
+
+
+export default function FullName(props) {
+    return (
+        <p>{props.firstname} {props.lastname}</p>
+    )
+}
---
+++ src/components/Title/index.js
@@ -0,0 +1,9 @@
+import './Title.css';
+
+
+export default function Title(props) {
+    return (
+        <h1>{props.text.toUpperCase()}</h1>
+    )
+}
+
