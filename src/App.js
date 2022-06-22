01dff7f8f9056e50b49469df6349f3a6f38a254c
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-06-21 19:28:42 -0500

    add component form

--- src/App.js
+++ src/App.js
@@ -75,26 +75,7 @@ function App() {
       <div className="main-container">
         <div className="container">{kodersUI}</div>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={firstName}
-          onChange={(event) => setFirstName(event.target.value)}
-        />
-        <input
-          value={lastName}
-          onChange={(event) => setLastName(event.target.value)}
-        />
-        <input value={age} onChange={(event) => setAge(event.target.value)} />
-        <input
-          value={gender}
-          onChange={(event) => setGender(event.target.value)}
-        />
-        <input
-          value={photoURL}
-          onChange={(event) => setPhotoURL(event.target.value)}
-        />
-        <button type="submit">Agregar Koder</button>
-      </form>
+      <Form></Form>
     </div>
   );
 }
