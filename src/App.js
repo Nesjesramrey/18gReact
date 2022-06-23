18bdf1621796adbec919226b0f45f3c01c2e2ebf
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-06-22 21:56:49 -0500

    routes and nested

--- src/App.js
+++ src/App.js
@@ -1,182 +1,27 @@
-// Components
-import Card from "./components/Card";
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useState } from "react";
+import AppContainer from "./containers/AppContainer";
+
+// Components
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [gender, setGender] = useState("");
-  const [photoURL, setPhotoURL] = useState("");
-  const [amount, setAmount] = useState(null);
-
-  const [cvc, setCvc] = useState("");
-  const [expiry, setExpiry] = useState("12/06");
-  const [name, setName] = useState("Name");
-  const [number, setNumber] = useState("");
-
-  const handleInputCvc = ({ target: { value } }) => {
-    const newAmount = value;
-    setCvc(newAmount);
-  };
-  const handleInputExpiry = ({ target: { value } }) => {
-    const newAmount = value;
-    setExpiry(newAmount);
-  };
-  const handleInputName = ({ target: { value } }) => {
-    const newAmount = value;
-    setName(newAmount);
-  };
-
-  const handleInputNumber = ({ target: { value } }) => {
-    const newAmount = value;
-    setNumber(newAmount);
-  };
-  const [koders, setKoders] = useState([
-    {
-      firstName: "Luis",
-      lastName: "Vera",
-      age: 24,
-      gender: "m",
-      photoURL: "https://sgame.etsisi.upm.es/pictures/18253.jpg?1621958969/",
-    },
-    {
-      firstName: "Yusef",
-      lastName: "Lopéz",
-      age: 40,
-      gender: "f",
-      photoURL: "https://media4.giphy.com/media/5sYyfIMRcpJWNqdySh/giphy.gif",
-    },
-  ]);
-
-  const kodersUI = koders.map(
-    ({ firstName, lastName, age, gender, photoURL }, index) => {
-      //console.log(firstName);
-      return (
-        <Card
-          key={index}
-          photoURL={photoURL}
-          firstName={firstName}
-          lastName={lastName}
-          age={age}
-          gender={gender}
-        />
-      );
-    }
-  );
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    //console.log("AGREGAR KODER");
-    const newKoders = [
-      ...koders,
-      {
-        firstName,
-        lastName,
-        age,
-        gender,
-        photoURL,
-      },
-    ];
-
-    setKoders(newKoders);
-
-    setFirstName("");
-    setLastName("");
-    setAge("");
-    setGender("");
-    setPhotoURL("");
-  };
-
-  //console.log(koders, "KODERS");
-
-  const USD = 20.16;
-  const handleChangeAmount = ({ target: { value } }) => {
-    const newAmount = Number(value);
-    setAmount(newAmount);
-  };
-
   return (
     <div className="App">
-      <div className="main-container">
-        <div className="container">{kodersUI}</div>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="First name"
-          value={firstName}
-          onChange={(event) => setFirstName(event.target.value)}
-        />
-        <input
-          value={lastName}
-          onChange={(event) => setLastName(event.target.value)}
-        />
-        <input value={age} onChange={(event) => setAge(event.target.value)} />
-        <input
-          value={gender}
-          onChange={(event) => setGender(event.target.value)}
-        />
-        <input
-          value={photoURL}
-          onChange={(event) => setPhotoURL(event.target.value)}
-        />
-        <button type="submit">Agregar Koder</button>
-      </form>
-
-      <div>
-        <input
-          type="number"
-          placeholder={!amount ? "0.00" : (amount / USD).toFixed(2)}
-          onChange={handleChangeAmount}
-        />
-        <input
-          type="number"
-          placeholder={!amount ? "0.00" : (amount * USD).toFixed(2)}
-          onChange={handleChangeAmount}
-        />
-
-        <div className=" paymentCard">
-          <div className="wrap">
-            <div className="tarjeta-wrap">
-              <div className="tarjeta">
-                <div className="adelante card1">
-                  <p>{number}</p>
-                  <p>{name}</p>
-                  <p>{expiry}</p>
-                </div>
-                <div className="atras">
-                  <p>{cvc}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <form className="containerInput">
-            <input
-              type="number"
-              placeholder="Card Number"
-              value={number}
-              onChange={handleInputNumber}
-            />
-            <br></br>
-            <input type="text" value={name} onChange={handleInputName} />
-            <br></br>
-            <input type="text" value={expiry} onChange={handleInputExpiry} />
-            <br></br>
-            <input type="number" value={cvc} onChange={handleInputCvc} />
-          </form>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<p>Landing page</p>} />
+        <Route path="login" element={<p>Login</p>} />
+        <Route path="signup" element={<p>Signup</p>} />
+        <Route path="app" element={<AppContainer />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-// 1. El usuario escribe la cantidad NUMERICA EN PESOS MEXICANOS en el input
-// 3. Se renderiza el valor en dolares
-
-// Notas:
-// El input no puede recibir letras solo numeros
-// El output tiene que estar formateado a dos decimales
