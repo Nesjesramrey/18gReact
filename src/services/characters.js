c03f73334b30dbe9efb5bcd3203b756eaa363362
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-06-27 18:14:24 -0500

    change link API

--- src/services/characters.js
+++ src/services/characters.js
@@ -1,5 +1,9 @@
 export const getCharacters = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  var page = Math.floor(Math.random() * (35 + 1 - 1) + 1);
+  console.log(page);
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
   const data = await response.json();
   return data;
 };
